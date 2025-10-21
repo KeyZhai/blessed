2c46fa53de1055a02b6195e4aff79844a9812f62
changhaozhai <feeaizch1001@gmail.com>
2025-10-22 01:40:14 +0800

    feat:add import md

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,11 +1,12 @@
 import { useTranslation } from "@/app/i18n/index";
 interface PageProps {
-  params: {
+  params: Promise<{
     lng: string;
-  };
+  }>;
 }
 
-export default async function Page({ params: { lng } }: PageProps) {
+export default async function Page({ params }: PageProps) {
+  const { lng } = await params;
   const { t } = await useTranslation(lng);
   return (
     <div className="note--empty-state">
